a49c57dc77c5712a0b5c925aba0f2d3c30b5df81
Aaru-06 <arumugampillai0610@gmail.com>
2020-06-07 22:07:11 +0530

    Updates

---
+++ precache-manifest.ee58c1d530d37dea2e116f5d0d04784d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba34fcb873a7ba7239004b93fd5ccf48",
+    "url": "/random-cp/index.html"
+  },
+  {
+    "revision": "233414c3ca0e06e81a3f",
+    "url": "/random-cp/static/css/main.e85ff897.chunk.css"
+  },
+  {
+    "revision": "9003f146596521c2e219",
+    "url": "/random-cp/static/js/2.3e2b0d2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-cp/static/js/2.3e2b0d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233414c3ca0e06e81a3f",
+    "url": "/random-cp/static/js/main.95cb9623.chunk.js"
+  },
+  {
+    "revision": "43fa930ff88fffc95237",
+    "url": "/random-cp/static/js/runtime-main.7ed0fa76.js"
+  }
+]);
